7286a73697e5cf9ab09b1d889024fa57ed337aa7
abhayyy30 <abhayyy.30@gmail.com>
2023-05-07 10:09:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var a=document.querySelector("h1");
+var color1=document.querySelector(".class1");
+var color2=document.querySelector(".class2")
+var body=document.getElementById("abc")
+function setgradient(){
+    body.style.background="linear-gradient(to right,"+ color1.value +","+ color2.value +")";
+}
+color1.addEventListener("input",setgradient)
+color2.addEventListener("input",setgradient)
